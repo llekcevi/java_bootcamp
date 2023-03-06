3181c65f855cef878c67974eb732b2b0316e84e1
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-03-06 17:58:50 +0100

    first homework assignment

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+import animals.Animal;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Class HelloWorld");
+        Animal.main(args);
+    }
+}
---
+++ animals/Animal.java
@@ -0,0 +1,7 @@
+package animals;
+
+public class Animal {
+    public static void main(String[] args){
+        System.out.println("Class Animal");
+    }
+}
