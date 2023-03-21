b5e52539a65e20146cbf2521d6f8017947b92bb6
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-03-21 13:33:48 +0100

    Basic functionality without bonus

--- fifth/Evidencija.java
+++ fifth/Evidencija.java
@@ -1,29 +1,47 @@
 package fifth;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Evidencija {
     ArrayList<Student> listaStudenata;
 
     public Evidencija() {
-
+        listaStudenata = new ArrayList<Student>();
     }
 
-    public void dodajStudenta(Student student) {
+    public void dodajStudenta(Scanner scanner) {
+
+        System.out.println("Unesite ime: ");
+        scanner.nextLine();
+        String ime = scanner.nextLine();
+        System.out.println("Unesite prezime: ");
+        String prezime = scanner.nextLine();
+        System.out.println("Unesite godinu studija: ");
+        int godinaStudija = scanner.nextInt();
+        System.out.println("Unesite broj indexa: ");
+        String brojIndexa = scanner.next();
+
+        Student student = new Student(ime, prezime, godinaStudija, brojIndexa);
         listaStudenata.add(student);
+        System.out.println("Student dodan.");
+
     }
 
-    public Student pretraziPoBrojuIndexa(String brojIndexa) {
+    public void pretraziPoBrojuIndexa(String brojIndexa) {
         for (Student student : listaStudenata) {
-            if (student.getBrojIndexa() == brojIndexa) {
-                return student;
+            if (student.getBrojIndexa().equals(brojIndexa)) {
+                System.out.println("Student pronađen: " + student.getIme() + " " + student.getPrezime());
             }
 
         }
-        return null;
     }
 
-    public ArrayList<Student> ispisiSveStudente(){
-        return listaStudenata;
+    public void ispisiSveStudente() {
+        System.out.println("Popis svih studenata: ");
+
+        for (Student student : listaStudenata) {
+            System.out.println(student.getIme() + " " + student.getPrezime());
+        }
     }
 }
--- fifth/fifth.java
+++ fifth/fifth.java
@@ -1,7 +1,42 @@
 package fifth;
 
+import java.util.Scanner;
+
 public class fifth {
     public static void main(String[] args) {
+        Evidencija e = new Evidencija();
+        boolean isTerminated = false;
+    
+        Scanner scanner = new Scanner(System.in);
 
+        while (!isTerminated) {
+
+            System.out.println("Odaberite opciju:");
+            System.out.println("1. Dodaj studenta");
+            System.out.println("2. Pretraga po indeksu");
+            System.out.println("3. Ispiši sve studente");
+            System.out.println("4. Izlaz");
+            int choice = scanner.nextInt();
+            switch (choice) {
+                case 1:
+                    e.dodajStudenta(scanner);
+                    break;
+                case 2:
+                    System.out.println("Upiši broj indeksa: ");
+                    String brojIndeksa = scanner.next();
+                    e.pretraziPoBrojuIndexa(brojIndeksa);
+                    break;
+                case 3:
+                    e.ispisiSveStudente();
+                    break;
+                case 4:
+                    isTerminated = true;
+                    break;
+                default:
+                    System.out.println("Došlo je do pogreške");
+            }
+        }
+        System.out.println("Doviđenja");
+        scanner.close();
     }
 }
