614edc60db1a053420d028c87ded3a155b6194c4
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-03-23 10:53:24 +0100

    Added functions for profesor

--- fifth/Evidencija.java
+++ fifth/Evidencija.java
@@ -5,9 +5,25 @@ import java.util.Scanner;
 
 public class Evidencija {
     ArrayList<Student> listaStudenata;
+    ArrayList<Profesor> listaProfesora;
 
     public Evidencija() {
         listaStudenata = new ArrayList<Student>();
+        listaProfesora = new ArrayList<Profesor>();
+    }
+
+    public void dodajProfesora(Scanner scanner){
+        System.out.println("Unesite ime: ");
+        scanner.nextLine();
+        String ime = scanner.nextLine();
+        System.out.println("Unesite prezime: ");
+        String prezime = scanner.nextLine();
+        System.out.println("Unesite predmet: ");
+        String predmet = scanner.nextLine();
+
+        Profesor profesor = new Profesor(ime, prezime, predmet);
+        listaProfesora.add(profesor);
+        System.out.println("Profesor dodan");
     }
 
     public void dodajStudenta(Scanner scanner) {
@@ -32,8 +48,9 @@ public class Evidencija {
         for (Student student : listaStudenata) {
             if (student.getBrojIndexa().equals(brojIndexa)) {
                 System.out.println("Student pronađen: " + student.getIme() + " " + student.getPrezime());
+            } else {
+                System.out.println("Ne postoji broj indeksa: " + brojIndexa);
             }
-
         }
     }
 
@@ -44,4 +61,23 @@ public class Evidencija {
             System.out.println(student.getIme() + " " + student.getPrezime());
         }
     }
+
+    public void ispisiSveProfesore() {
+        System.out.println("Popis svih profesora: ");
+
+        for (Profesor profesor : listaProfesora) {
+            System.out.println(profesor.getIme() + " " + profesor.getPrezime() + " - " + profesor.getPredmet());
+        }
+    }
+
+    public void pretraziProfesorePoPredmetu(String predmet) {
+        for (Profesor profesor : listaProfesora) {
+            if (profesor.getPredmet().equals(predmet)) {
+                System.out.println("Predmet predaje " + profesor.getIme() + " " + profesor.getPrezime());
+            } else {
+                System.out.println("Nitko ne predaje navedeni predmet. ");
+            }
+        }
+    }
+
 }
--- fifth/fifth.java
+++ fifth/fifth.java
@@ -6,16 +6,21 @@ public class fifth {
     public static void main(String[] args) {
         Evidencija e = new Evidencija();
         boolean isTerminated = false;
-    
+
         Scanner scanner = new Scanner(System.in);
 
         while (!isTerminated) {
-
+            System.out.println("----------------------");
             System.out.println("Odaberite opciju:");
             System.out.println("1. Dodaj studenta");
-            System.out.println("2. Pretraga po indeksu");
+            System.out.println("2. Pretraži studente po indeksu");
             System.out.println("3. Ispiši sve studente");
-            System.out.println("4. Izlaz");
+            System.out.println("4. Dodaj profesora");
+            System.out.println("5. Ispiši sve profesore");
+            System.out.println("6. Pretraži profesore po predmetu");
+            System.out.println("7. Izlaz");
+            System.out.println("----------------------");
+
             int choice = scanner.nextInt();
             switch (choice) {
                 case 1:
@@ -23,13 +28,25 @@ public class fifth {
                     break;
                 case 2:
                     System.out.println("Upiši broj indeksa: ");
-                    String brojIndeksa = scanner.next();
+                    String brojIndeksa = scanner.nextLine();
                     e.pretraziPoBrojuIndexa(brojIndeksa);
                     break;
                 case 3:
                     e.ispisiSveStudente();
                     break;
                 case 4:
+                    e.dodajProfesora(scanner);
+                    break;
+                case 5:
+                    e.ispisiSveProfesore();
+                    break;
+                case 6:
+                    System.out.println("Upiši predmet: ");
+                    scanner.nextLine();
+                    String predmet = scanner.nextLine();
+                    e.pretraziProfesorePoPredmetu(predmet);
+                    break;
+                case 7:
                     isTerminated = true;
                     break;
                 default:
