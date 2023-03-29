f2b5b855857fe0bea096233a972cfd348f60ecd6
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-03-29 15:58:20 +0200

    Added javadoc and implementation of deleteVehicle

--- Fleet/VehicleManagerImpl.java
+++ Fleet/VehicleManagerImpl.java
@@ -1,10 +1,19 @@
 package Fleet;
 
 import java.util.ArrayList;
-
+/**
+ *
+ * Implements {@link VehicleManager} interface.
+ *  
+ */
 public class VehicleManagerImpl implements VehicleManager {
+    /**
+     * Represents a fleet of vehicles.
+     */
     private ArrayList<Vehicle> fleet;
-
+    /**
+     * Constructs VehicleManagerImpl object that manages vehicles in a fleet.
+     */
     public VehicleManagerImpl(){
         fleet = new ArrayList<Vehicle>();
     }
@@ -33,7 +42,8 @@ public class VehicleManagerImpl implements VehicleManager {
     }
 
     @Override
-    public void deleteVehicle() {
+    public void deleteVehicle(int VIN) {
+           fleet.removeIf(v -> (v.getVIN() == VIN)); 
     }
 
 }
