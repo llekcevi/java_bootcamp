63537a2cca31717698f2a7135d7392f8a648e8fe
Luka Lekcevic <luka.lekcevi@gmail.com>
2023-03-29 15:57:55 +0200

    Testing

--- Fleet/home.java
+++ Fleet/home.java
@@ -4,8 +4,15 @@ public class home {
     public static void main(String[] args) {
         VehicleManagerImpl manager = new VehicleManagerImpl();
 
-        manager.addVehicle(new Car("audi", "a4", 2022, "black", 1234, "diesel", 4, "sedan"));
-        manager.addVehicle(new Truck("ford", "f-150", 2016, "red", 4321, "diesel", 150.0));
+        Car audi = new Car("audi", "a4", 2022, "black", 1234, "diesel", 4, "sedan");
+        Truck ford = new Truck("ford", "f-150", 2016, "red", 4321, "diesel", 150.0);
+        
+        manager.addVehicle(ford);
+        manager.addVehicle(audi);
+        
         manager.printAllVehicles();
+        manager.deleteVehicle(1234);
+        manager.printAllVehicles();
+        
     }
 }
